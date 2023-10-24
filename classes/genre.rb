ef9108b757685a859ba4cf301f4162754baf0c79
coker2019 <olabisi2k11@gmail.com>
2023-10-24 15:08:02 +0100

    add genre class

---
+++ classes/genre.rb
@@ -0,0 +1,17 @@
+require '../item'
+
+
+class Genre
+  attr_accessor :id, :name, :items
+
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    item.genre = self
+    @items << item
+  end
+end
