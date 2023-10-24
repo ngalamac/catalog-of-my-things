303c6eb8520f58671058207d90afb83c39f4e6b9
coker2019 <olabisi2k11@gmail.com>
2023-10-24 16:25:01 +0100

    update add_music

--- options/add_music_album.rb
+++ options/add_music_album.rb
@@ -1,24 +1,37 @@
- require './classes/music_album'
- require './classes/label'
 
+require './options/add_props'
+require './classes/musicalbum'
 
- def add_music_album
-    puts 'What is the title of the music album?'
-    title = gets.chomp
-  
-    puts 'Who is the artist of the music album?'
-    artist = gets.chomp
-  
-    puts 'What is the release date of the music album? (YYYY-MM-DD)'
-    publish_date = gets.chomp
-  
-    puts 'What record label does the music album belong to?'
-    label = gets.chomp
-  
-    album = MusicAlbum.new(title, artist, Date.parse(publish_date), label)
-    @albums << album
-  
-    save_music_albums_json
-  
-    puts 'Music album created successfully!'
+module AddMusicAlbum
+  include AddProps
+
+  def add_music_album
+    puts 'Enter publish date (yyyy-mm-dd):'
+
+    loop do
+      @publish_date = gets.chomp
+      break if @publish_date =~ /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/
+
+      puts "Provide the date in format 'yyyy-mm-dd'"
+    end
+
+    puts 'Available on Spotify? (Y/y/N/n):'
+
+    loop do
+      @on_spotify = gets.chomp
+      break if @on_spotify =~ /^[YyNn]/
+
+      puts 'Provide valid input (Y/y/N/n)'
+    end
+
+    @on_spotify = @on_spotify.downcase == 'y'
+    album = MusicAlbum.new(@publish_date, @on_spotify)
+    @music_albums << album
+
+    choose_label(album)
+    choose_genre(album)
+    choose_author(album)
+
+    puts "Album added successfully!\n"
   end
+end
